80a598ce12a09c390f9ec374972652fade86b2bf
antonioamaralsantos-ship-it <antonio.amaral.santos@escola.pr.gov.br>
2026-03-24 15:22:56 +0000

    variaveis

---
+++ script.js
@@ -0,0 +1,15 @@
+// var
+
+var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+
+console.log(area);
+
+// let
+
+let forma = 'retângulo';
+let altura = 5; 
+let comprimento = 7;
+let area;
